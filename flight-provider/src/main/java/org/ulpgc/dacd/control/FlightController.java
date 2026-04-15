1b0ece9240b9141734f4defc1f9bea7847821369
Cesl78 <cesluisvs78@gmail.com>
2026-04-15 16:16:13 +0100

    Creación de FlightController para manejar tareas programadas

---
+++ flight-provider/src/main/java/org/ulpgc/dacd/control/FlightController.java
@@ -0,0 +1,30 @@
+package org.ulpgc.dacd.control;
+
+import org.ulpgc.dacd.control.storage.FlightDatabaseManager;
+import org.ulpgc.dacd.model.Flight;
+import java.util.List;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
+
+public class FlightController {
+    private final FlightProvider flightProvider;
+    private final FlightDatabaseManager databaseManager;
+
+    public FlightController(FlightProvider flightProvider, FlightDatabaseManager databaseManager) {
+        this.flightProvider = flightProvider;
+        this.databaseManager = databaseManager;
+    }
+
+    public void execute() {
+        ScheduledExecutorService scheduler = Executors.newSingleThreadScheduledExecutor();
+        scheduler.scheduleAtFixedRate(() -> {
+            try {
+                List<Flight> flights = flightProvider.getFlights();
+                databaseManager.saveFlights(flights);
+            } catch (Exception e) {
+                System.err.println(e.getMessage());
+            }
+        }, 0, 1, TimeUnit.HOURS);
+    }
+}
